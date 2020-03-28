24c6c27857dffa932d202b953db7ee4bd65c5cd2
hossg <disp1975@icloud.com>
2020-03-28 09:35:09 +0000

    added a simple example webservice to allow demonstration of obf proxy capabilities

---
+++ examples/retrieve_service.py
@@ -0,0 +1,10 @@
+from bottle import route, run, request, response, static_file
+
+# A super simple dummy web service that will retrieve a resource and return it to the caller.
+# Used only to allow demonstration of the obf proxy service
+
+@route('/retrieve/<file:path>')
+def server_static(file):
+    return static_file(file,root='.')
+
+run(host='localhost', port=8080, debug=True)
